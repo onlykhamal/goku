1760ae9babf5855530e2a26268a68216143038db
onlykhamal <kammalannan123@gmail.com>
2025-05-24 03:30:02 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const cacheName = "khamal-list-cache-v1";
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
+// Install
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
